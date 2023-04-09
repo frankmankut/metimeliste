76fdf082eccdf681a05b2d0e396cba1b13b3c899
frankmankut <workingclass.lib@protonmail.com>
2023-04-09 20:36:22 +0200

    Added functio "Add New line" and "Remove line".

--- src/components/App.js
+++ src/components/App.js
@@ -7,28 +7,65 @@ import React, { useState } from 'react';
 import uniqid from 'uniqid';
 
 function App() {
-  const [weekDays, setWeekDays] = useState(['Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag', 'Søndag']);
+  const [weekDays, setWeekDays] = useState([{ Mandag: [{}] }, { Tirsdag: [{}] }, { Onsdag: [{}] }, { Torsdag: [{}] }, { Fredag: [{}] }, { Lørdag: [{}] }, { Søndag: [{}] },]);
+
+  function addDay(day, dayCell) {
+    const newWeekDays = weekDays.map((dayx) => {
+      if (Object.keys(dayx)[0] === day) {
+        const newWeekDay = Object.values(dayx)[0]
+        newWeekDay.push(dayCell)
+        return { [day]: newWeekDay }
+      } else {
+        return dayx
+      }
+
+    })
+    setWeekDays(newWeekDays)
+    
+
+  }
+
+  function removeDay(day, id) {
+    const newWeekDays = weekDays.map((dayx) => {
+      if (Object.keys(dayx)[0] === day) {
+        const newWeekDay = Object.values(dayx)[0].filter((dayy) => dayy.id !== id)
+        return { [day]: newWeekDay }
+      } else {
+        return dayx
+      }
+    })
+    setWeekDays(newWeekDays)
+  }
   return (
 
 
     <div className='container'>
-      <MainInfo/>
-      {weekDays.map((day)=>{
-        return (
-          <WeekDays key={(uniqid())} day={day}/>
-        )
+      <MainInfo />
+      {weekDays.map((day) => {
+        return Object.values(day)[0].map((days) => {
+          return (
+            <WeekDays
+              key={uniqid()}
+              dayIndex={Object.values(day)[0].indexOf(days)}
+              dayName={Object.keys(day)[0]}
+              id={days.id}
+              addDay={addDay}
+              removeDay={removeDay}
+            />
+          )
+        })
       }
       )}
-      <AllSum/>
-      
-
-      
+      <AllSum />
 
 
-      
 
 
-      
+
+
+
+
+
 
     </div>
 
--- src/components/WeekDays.js
+++ src/components/WeekDays.js
@@ -1,63 +1,73 @@
 import '../styles/App.css';
-
+import uniqid from 'uniqid';
 function WeekDays(props) {
     return (
-        
-            <div className='rectangle'>
-                <div className='cell'>
-                    <span>{props.day}</span> <button type='button' onClick={() => alert("En ny linje dukker opp.")}>Legg til en linje</button>
 
-                    <input className='inDate' name='Date' type="date" placeholder="dd/mm/yyyy" lang='no'></input>
+        <div className='rectangle'>
+            <div className='cell'>
+                <span>{props.dayName}</span>
 
-                </div>
-                <div className='cell'>
+                {props.dayIndex === 0 &&
 
-                    <input className='oNrmesse' name='oNumer'></input>
+                    <button type='button' onClick={() => props.addDay(props.dayName,{id: uniqid()})}>Legg til en linje</button>
+                }
 
-                </div>
-                <div className='cell'>
+                {props.dayIndex !== 0 &&
 
-                    <input className='oNrmesse' name='Messe'></input>
+                    <button type='button' onClick={() => props.removeDay(props.dayName,props.id)}>Slett linjen</button>
+                }
 
-                </div>
-                <div className='cell'>
+                <input className='inDate' name='Date' type="date" placeholder="dd/mm/yyyy" lang='no'></input>
 
-                    <input type="checkbox" className='checkVanlig' name="Vanlig"></input>
-
-                </div>
-                <div className='cell'>
-
-                    <input type="time" className='tid' name="startTid"></input>
-
-                </div>
-                <div className='cell'>
-
-                    <input type="time" className='tid' name="sluttidtTid"></input>
-
-                </div>
-                <div className='cell'></div>
-                <div className='cell'></div>
-                <div className='overCell'>
-                    <div className='midCell'></div>
-                    <div className='midCell1'></div>
-                </div>
-                <div className='cell'>
-
-                    <input type="checkbox" className='checkVanlig' name="Hellig" onClick={() => alert("8 timer")}></input>
-
-                </div>
-                <div className='cell'>
-
-                    <input type="text" className='matNok' name='matNok'></input>&nbsp;
-                    <label htmlFor="matNok">NOK</label>
-
-
-                </div>
             </div>
+            <div className='cell'>
+
+                <input className='oNrmesse' name='oNumer'></input>
+
+            </div>
+            <div className='cell'>
+
+                <input className='oNrmesse' name='Messe'></input>
+
+            </div>
+            <div className='cell'>
+
+                <input type="checkbox" className='checkVanlig' name="Vanlig"></input>
+
+            </div>
+            <div className='cell'>
+
+                <input type="time" className='tid' name="startTid"></input>
+
+            </div>
+            <div className='cell'>
+
+                <input type="time" className='tid' name="sluttidtTid"></input>
+
+            </div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='overCell'>
+                <div className='midCell'></div>
+                <div className='midCell1'></div>
+            </div>
+            <div className='cell'>
+
+                <input type="checkbox" className='checkVanlig' name="Hellig" onClick={() => alert("8 timer")}></input>
+
+            </div>
+            <div className='cell'>
+
+                <input type="text" className='matNok' name='matNok'></input>&nbsp;
+                <label htmlFor="matNok">NOK</label>
 
 
-            
-        
+            </div>
+        </div>
+
+
+
+
 
     )
 
