ee2dff078ee27ebe9cad57fa02bfacdbcbe44b1e
frankmankut <workingclass.lib@protonmail.com>
2023-04-14 20:26:18 +0200

    Added new class to change the text color in cells "Sum"

--- src/components/AllSum.js
+++ src/components/AllSum.js
@@ -3,20 +3,20 @@ import '../styles/App.css';
 function AllSum() {
     return (
         <div className='rectangle'>
-        <div className='cell1'>Sum</div>
-        <div className='cell1'></div>
-        <div className='cell1'></div>
-        <div className='cell1'></div>
-        <div className='cell1'></div>
-        <div className='cell1'></div>
-        <div className='cell1'></div>
-        <div className='cell1'></div>
+        <div className='cell2'>Sum</div>
+        <div className='cell2'></div>
+        <div className='cell2'></div>
+        <div className='cell2'></div>
+        <div className='cell2'></div>
+        <div className='cell2'></div>
+        <div className='cell2'></div>
+        <div className='cell2'></div>
         <div className='overCell'>
           <div className='midCell'></div>
           <div className='midCell1'></div>
         </div>
         <div className='cell'></div>
-        <div className='cell1'>NOK</div>
+        <div className='cell2'>NOK</div>
       </div>
         
     )
